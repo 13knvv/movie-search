5aedea54cc9fe13d2617fb2305fc7318d23081b6
Vitalii Kniazev <13.kn.v.v@gmail.com>
2022-06-01 15:47:35 +0300

    something

--- src/Components/common/Movie/Movie.tsx
+++ src/Components/common/Movie/Movie.tsx
@@ -17,25 +17,64 @@ const Movie = () => {
     }
   }, [])
 
-  const namesMovie = [movie.nameRu, movie.nameEn, movie.nameOriginal].filter((name) => {
+  const namesMovie = [movie.nameRu, movie.nameEn, movie.nameOriginal].filter(name => {
     if (name) return name
   }).join(' / ')
+
+  const countries = movie.countries?.map(item => item.country).join(', ')
+  const genres = movie.genres?.map(item => item.genre).join(', ')
+  const ageLimits = movie.ratingAgeLimits?.slice(3) + '+'
   
   return (
     <div>
       <div className={s.posterInfoWrapp}>
         <div className={s.poster}>
-          <img src={movie.posterUrlPreview} alt="" />
+          <img src={movie.posterUrl} alt={namesMovie} />
         </div>
         <div className={s.info}>
-          {namesMovie}
-          {movie.description}
+          <h3>{namesMovie}</h3>
+          { movie.ratingKinopoisk && 
+                                  <div>Рейтинг Kinopoisk: {movie.ratingKinopoisk} /
+                                    {movie?.ratingKinopoiskVoteCount} голосов
+                                  </div>
+          }
+
+          { movie.ratingImdb &&  
+                            <div>Рейтинг Imdb: {movie.ratingImdb} / 
+                              {movie?.ratingImdbVoteCount} голосов
+                            </div>
+          }
+
+          { movie.year && 
+                      <div>Год: {movie.year}</div>
+          }
+
+          { countries && 
+                      <div>Страна: {countries}</div>
+          }
+
+          { genres && 
+                    <div>Жанр: {genres}</div>
+          }
+
+          { movie.filmLength &&
+                    <div>Продолжительность: {movie.filmLength + ' мин.'}</div>
+          }
+          
+          { ageLimits &&
+                    <div>Возрастные ограничения: {ageLimits}</div>
+          }
+          
+          { movie.description && 
+                    <div>{movie.description}</div>
+          }
+          
         </div>
         
       </div>
       
       <div className={s.coverUrl}>
-        <img src={movie.coverUrl} alt="" />
+        <img src={movie.coverUrl} alt={namesMovie} />
       </div>
     </div>
   )
